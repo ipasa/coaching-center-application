ebee3b6eac134f4b943a6843a5f80dfdd5d457fd
Hasan Hafiz Pasha - Main Workstation <mach.pasha@gmail.com>
2016-08-31 15:15:20 +0000

    Add edit, Udate & restroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts  = Post.all.order('created_at DESC')
+    @posts  = Post.all.order("created_at DESC")
   end
 
   def new
@@ -14,11 +14,30 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
-
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title,:body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
