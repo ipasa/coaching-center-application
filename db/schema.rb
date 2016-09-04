0103662be9b3a6c28e9a48896d2e764f4a474943
Hasan Hafiz Pasha <mac_pasha@ovi.com>
2016-09-04 20:06:50 +0600

    Adding user name option on signup form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903150345) do
+ActiveRecord::Schema.define(version: 20160903153641) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(version: 20160903150345) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
   add_foreign_key "comments", "posts"
